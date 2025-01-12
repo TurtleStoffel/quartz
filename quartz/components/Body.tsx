56f7336b74ae7c5a3d350a1aeda658449f38cb9e
TurtleStoffel <me@turtlestoffel.com>
2025-01-12 16:06:38 +0100

    Remove unnecessary darkmode script from `Body.tsx`

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,6 @@
 // @ts-ignore
 import clipboardScript from "./scripts/clipboard.inline"
 import clipboardStyle from "./styles/clipboard.scss"
-import darkmodeScript from "./scripts/darkmode.inline"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
@@ -9,7 +8,6 @@ const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
 }
 
 Body.afterDOMLoaded = clipboardScript
-Body.beforeDOMLoaded = darkmodeScript
 Body.css = clipboardStyle
 
 export default (() => Body) satisfies QuartzComponentConstructor
